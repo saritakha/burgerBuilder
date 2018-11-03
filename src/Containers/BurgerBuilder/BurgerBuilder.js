c6736f7921717300306cf74c6a200ccfd86626ea
sarita khanal <saritakhanal533@gmail.com>
2018-11-03 21:16:00 +0200

    modal for order summary added

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Aux from './../../hoc/Aux';
 import Burger from '../../Components/Burger/Burger';
 import BuildControls from '../../Components/Burger/BuildControls/BuildControls';
 import Modal from '../../Components/UI/Modal/Modal';
+import OrderSummary from '../../Components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -87,7 +88,9 @@ class BurgerBuilder extends Component {
       }
       return(
         <Aux>
-          <Modal />
+          <Modal>
+            <OrderSummary ingredients = {this.state.ingredients} />
+          </Modal>
           <Burger ingredients= { this.state.ingredients }/>
           <BuildControls
             ingredientAdded = { this.addIngredientHandler } 
